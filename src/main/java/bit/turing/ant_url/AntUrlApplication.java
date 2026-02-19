c5093920ea149759997ec2033748c58d189023ec
euclides <euclidesfreiresa@gmail.com>
2026-02-18 23:29:59 -0300

    first commit

---
+++ src/main/java/bit/turing/ant_url/AntUrlApplication.java
@@ -0,0 +1,13 @@
+package bit.turing.ant_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bit/turing/ant_url/AntUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package bit.turing.ant_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
