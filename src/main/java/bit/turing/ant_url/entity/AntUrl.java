17036f983c8ce3d8822894288558beae6f7374e3
euclides <euclidesfreiresa@gmail.com>
2026-03-26 23:53:27 -0300

    entity: anturl

---
+++ src/main/java/bit/turing/ant_url/entity/AntUrl.java
@@ -0,0 +1,65 @@
+package bit.turing.ant_url.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "anturl")
+public class AntUrl {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", length = 512, nullable = false)
+    private String title;
+
+    @Column(name = "url", length = 4000, nullable = false)
+    private String url;
+
+    @Column(name = "domain", length = 1024, nullable = false)
+    private String domain;
+
+    @Column(name = "alias", length = 1024, nullable = false)
+    private String alias;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+}
